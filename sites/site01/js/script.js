b92d9e1a4510994ecfea92b581c0243fd7fe22cb
nareeshin <73747535+nareeshin@users.noreply.github.com>
2021-11-28 01:14:14 +0900

    클릭 이벤트 대성공

--- sites/site01/js/script.js
+++ sites/site01/js/script.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
 
 });
+
+$(function(){
+    $("#updateit").toggle(
+     function(){
+       $('.bar').css("width", '+=' + (0.1 * $('.progress').width()));
+       return false; 
+    },
+    function(){
+       $('.bar').css("width", '-=' + (0.1 * $('.progress').width()));
+       return false;
+    });
+  });
