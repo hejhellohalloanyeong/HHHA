0d75c2ec9c36fea3d04c0caca9bc449fbdfd3bfd
nareeshin <73747535+nareeshin@users.noreply.github.com>
2021-12-02 19:05:24 +0900

    클래스명, 아이디명 정리 끝! + A, B 업로드 완료
    B는 아직 덜완성

--- js/script.js
+++ js/script.js
@@ -1,40 +1,66 @@
+// Title
 
-// $(function(){
-//     $("#updateit").toggle(
-//      function(){
-//        $('.bar').css("width", '+=' + (0.6 * $('.bar').width()));
-//        return false; 
-//     },
-//     function(){
-//        $('.bar').css("width", '-=' + (0.6 * $('.bar').width()));
-//        return false;
-//     });
-//   });
+$('#main').on('click', function (e) {
 
+  if ($('#main').hasClass('item')) {
+    $('#main').removeClass('item').addClass('item_open');
+    $('#about, #contact, #alphabet, #audience, #birthday').addClass('item');
+  } else if ($('#main').hasClass('item_open')) {
+    $('#main').removeClass('item_open').addClass('item');
+  }
+});
+
+$('#about').on('click', function (e) {
+
+  if ($('#about').hasClass('item')) {
+    $('#about').removeClass('item').addClass('item_open');
+    $('#main, #contact, #alphabet, #audience, #birthday').addClass('item');
+  } else if ($('#about').hasClass('item_open')) {
+    $('#about').removeClass('item_open').addClass('item');
+  }
+});
+
+$('#alphabet').on('click', function (e) {
+
+  if ($('#alphabet').hasClass('item')) {
+    $('#alphabet').removeClass('item').addClass('item_open');
+    $(' #main, #about, #contact, #audience, #birthday').addClass('item');
+  } else if ($('#alphabet').hasClass('item_open')) {
+    $('#alphabet').removeClass('item_open').addClass('item');
+  }
+});
+
+$('#contact').on('click', function (e) {
+
+  if ($('#contact').hasClass('item')) {
+    $('#contact').removeClass('item').addClass('item_open');
+    $('#main, #about, #alphabet, #audience, #birthday').addClass('item');
+  } else if ($('#contact').hasClass('item_open')) {
+    $('#contact').removeClass('item_open').addClass('item');
+  }
+});
+
+
+// A - Z 
 
 $('#audience').on('click', function(e){
   
-   if ($('#audience').hasClass('item')) {
-       $('#audience').removeClass('item').addClass('item_60');
-       $('#birthday').addClass('item');
-   } else if ($('#audience').hasClass('item_60')) {
-     $('#audience').removeClass('item_60').addClass('item');
-   } 
-   });
+    if ($('#audience').hasClass('item')) {
+        $('#audience').removeClass('item').addClass('item_open');
+        $('#main, #about, #alphabet, #contact, #birthday').addClass('item');
+    } else if ($('#audience').hasClass('item_open')) {
+      $('#audience').removeClass('item_open').addClass('item');
+    } 
+    });
 
-
-
-   // $('#audience').on('click', function(){
-   //    $('#audience').removeClass('item');
-   //    $('#birthday').addClass('item');
-   //  });
-
-   $('#birthday').on('click', function(e){
+$('#birthday').on('click', function(e){
   
       if ($('#birthday').hasClass('item')) {
-          $('#birthday').removeClass('item').addClass('item_60');
-          $('#audience').addClass('item');
-      } else if ($('#birthday').hasClass('item_60')) {
-        $('#birthday').removeClass('item_60').addClass('item');
+          $('#birthday').removeClass('item').addClass('item_open');
+          $('#main, #about, #alphabet, #contact, #audience').addClass('item');
+      } else if ($('#birthday').hasClass('item_open')) {
+        $('#birthday').removeClass('item_open').addClass('item');
       } 
       });
+
+    
